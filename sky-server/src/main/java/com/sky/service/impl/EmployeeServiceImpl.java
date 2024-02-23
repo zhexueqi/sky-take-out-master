f3535751ec005456ca45f1c70038f7245ec702b1
zhexueqi <zhexueqi189@gmail.com>
2024-02-23 22:08:50 +0800

    苍穹外卖初始代码

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -39,6 +39,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
         // TODO 后期需要进行md5加密，然后再进行比对
         if (!password.equals(employee.getPassword())) {
             //密码错误
