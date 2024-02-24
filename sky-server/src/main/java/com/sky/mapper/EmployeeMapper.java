1a113a5587c92f55cbe3e090f98ecdd43bad3694
zhexueqi <zhexueqi189@gmail.com>
2024-02-24 19:58:12 +0800

    员工分页查询业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -24,4 +26,8 @@ public interface EmployeeMapper {
     void insert(Employee employee);
 
 
+    /*
+    * 分页查询员工数据
+    * */
+    Page pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
