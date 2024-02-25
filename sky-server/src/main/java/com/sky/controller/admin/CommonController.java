01a313f65a9027c98aa37b18eff07b4ed7fcfdf0
zhexueqi <zhexueqi189@gmail.com>
2024-02-25 16:40:02 +0800

    文件上传代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,50 @@
+package com.sky.controller.admin;
+
+
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * @author zhexueqi
+ * @ClassName CommonController
+ * @since 2024/2/25    15:38
+ */
+@RestController
+@Slf4j
+@RequestMapping("/admin/common")
+public class CommonController {
+
+    @Autowired
+    private AliOssUtil aliOssUtil;
+
+
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file) {
+        log.info("上传文件:{}",file);
+
+        try {
+            //获取文件原始名字
+            String originalFilename = file.getOriginalFilename();
+            //获取文件后缀
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            //文件的名字
+            String fileName = UUID.randomUUID().toString() +  extension;
+            //文件的请求路径
+            String url = aliOssUtil.upload(file.getBytes(),fileName);
+            return Result.success(url);
+        } catch (IOException e) {
+            log.info("文件上传失败，{}",e);
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+}
